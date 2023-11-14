165e68c2e78acd1032d16bd50dbb744276a08ae6
Hanna <hanna-marii.kriisa@voco.ee>
2023-11-15 00:29:56 +0200

    Error page

--- public/index.php
+++ public/index.php
@@ -22,7 +22,19 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->get('/', 'App\Controller\AlbumsController:default');
+$app->get('/details/{id:[0-9]+}', 'App\Controller\AlbumsController:details');
 $app->get('/search', 'App\Controller\AlbumsController:search');
 $app->any('/form', 'App\Controller\AlbumsController:form');
+
+$errorMiddleware = $app->addErrorMiddleware(false, true, true);
+
+$errorMiddleware->setErrorHandler(
+    Slim\Exception\HttpNotFoundException::class,
+    function (Psr\Http\Message\ServerRequestInterface $request) use ($container) {
+        $controller = new App\Controller\ExceptionController($container);
+        return $controller->notFound($request);
+    }
+);
+
 
 $app->run();
--- src/Controller/AlbumsController.php
+++ src/Controller/AlbumsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpNotFoundException;
 
 class AlbumsController extends Controller
 {
@@ -33,6 +34,21 @@ class AlbumsController extends Controller
     ]);
 }
 
+public function details(Request $request, Response $response, $args = []) 
+{
+    $albums = json_decode(file_get_contents(__DIR__.'/../../data/albums.json'), true);
+
+    $key = array_search($args['id'], array_column($albums, 'id'));
+
+    if($key === false){
+    	throw new HttpNotFoundException($request, "Error");
+    }
+
+    return $this->render($response, 'details.html', [
+        'albums' => $albums[$key]
+    ]);
+}
+
 public function form(Request $request, Response $response) 
 {
     $albums = json_decode(file_get_contents(__DIR__.'/../../data/albums.json'), true);
@@ -43,7 +59,7 @@ public function form(Request $request, Response $response)
     if ($request->getMethod() === 'POST') {
         $postData = $request->getParsedBody();
         $query = $postData['q'] ?? $query;
-        
+
         $albums = array_values(array_filter($albums, function($album) use ($query){
             return strpos($album['title'], $query) !== false or strpos($album['artist'], $query) !== false;
         }));
